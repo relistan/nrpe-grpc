f05e9927687c29a7c2afc4b13dac76cb7b9d850c
Karl Matthias <karl.matthias@gonitro.com>
2017-03-13 21:00:03 +0000

    Working demo

---
+++ client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"github.com/relistan/nrpe-test/nrperpc"
+)
+
+const (
+	address = "localhost:50000"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := nrperpc.NewCheckClient(conn)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Pass the check name as an arg")
+	}
+
+	resp, err := c.NrpeCheck(context.Background(), &nrperpc.NrpeRequest{Name: os.Args[1]})
+	if err != nil {
+		log.Fatalf("could not call nrpe: %v", err)
+	}
+	log.Printf("%v: %v", resp.StatusCode, resp.StatusLine)
+}
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/envimate/nrpe"
+	"github.com/relistan/nrpe-test/nrperpc"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	address = ":50000"
+)
+
+type server struct{}
+
+func (s *server) NrpeCheck(ctx context.Context, request *nrperpc.NrpeRequest) (*nrperpc.NrpeReply, error) {
+	// TODO ignoring context
+
+	conn, err := net.Dial("tcp", "docker1:5666")
+	if err != nil {
+		return nil, err
+	}
+
+	command := nrpe.NewCommand(request.Name)
+
+	// ssl = true, timeout = 0
+	result, err := nrpe.Run(conn, command, true, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	reply := &nrperpc.NrpeReply{
+		StatusCode: int32(result.StatusCode),
+		StatusLine: result.StatusLine,
+	}
+	return reply, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	nrperpc.RegisterCheckServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
